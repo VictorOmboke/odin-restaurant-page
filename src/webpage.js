0dc848ac1f4f9cb3cef5f6d4dc8fd5923b86e87b
Victor Omboke <victoromboke@gmail.com>
2023-10-13 12:13:45 -0700

    Rework existing code to create header and header elements

---
+++ src/webpage.js
@@ -0,0 +1,44 @@
+function createHeader() {
+  const header = document.createElement("div");
+  header.classList.add("header");
+
+  const restaurantName = document.createElement("h1");
+  restaurantName.classList.add("restaurantName");
+  restaurantName.textContent = "Ōishii";
+
+  header.appendChild(restaurantName);
+  header.appendChild(createNav());
+
+  return header;
+}
+
+function createNav() {
+  const nav = document.createElement("div");
+  nav.classList.add("navContainer");
+
+  const home = document.createElement("button");
+  home.classList.add("homeBtn");
+  home.textContent = "Home";
+
+  const menu = document.createElement("button");
+  menu.classList.add("menuBtn");
+  menu.textContent = "Menu";
+
+  const contact = document.createElement("button");
+  contact.classList.add("contactBtn");
+  contact.textContent = "Contact";
+
+  nav.appendChild(home);
+  nav.appendChild(menu);
+  nav.appendChild(contact);
+
+  return nav;
+}
+
+function createWebpage() {
+  const content = document.getElementById("content");
+
+  content.appendChild(createHeader());
+}
+
+export default createWebpage;
