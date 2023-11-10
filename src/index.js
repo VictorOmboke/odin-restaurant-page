ef55fb1d350cdddf1337b3471385241a7cf88469
Victor Omboke <victoromboke@gmail.com>
2023-11-10 12:13:34 -0800

    Create functions to implement tab switching with imported js modules and render website

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
+import createHome from "./home";
+import createMenu from "./menu";
+import createContact from "./contact";
 import createWebpage from "./webpage";
 
-createWebpage();
+function clearContent() {
+  const content = document.getElementById("content");
+
+  while (content.children.length > 1) {
+    content.removeChild(content.lastChild);
+  }
+}
+
+function setActiveTab(tab) {
+  const tabs = document.querySelectorAll(".tab");
+  tabs.forEach((t) => t.classList.remove("active"));
+
+  tab.classList.add("active");
+}
+
+function createTabEvents() {
+  const tabs = document.querySelectorAll(".tab");
+
+  tabs.forEach((tab) => {
+    tab.addEventListener("click", () => {
+      clearContent();
+      setActiveTab(tab);
+
+      if (tab.classList.contains("home")) {
+        createHome();
+      } else if (tab.classList.contains("menu")) {
+        createMenu();
+      } else if (tab.classList.contains("contact")) {
+        createContact();
+      }
+    });
+  });
+}
+
+function LoadPage() {
+  createWebpage();
+  createHome();
+  createTabEvents();
+}
+
+LoadPage();
